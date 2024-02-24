d33c01fbef14b023ed0b3147123b075b56f42339
SergeiStrukov <serega.strukov21@gmail.com>
2024-02-25 01:59:33 +0400

    ser

--- src/main/java/org/example/examp240424/Helloworld.java
+++ src/main/java/org/example/examp240424/Helloworld.java
@@ -2,6 +2,6 @@ package org.example.examp240424;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!!!!!");
     }
 }
